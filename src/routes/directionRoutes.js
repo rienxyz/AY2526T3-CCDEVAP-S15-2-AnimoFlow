0328bece8a620bd6fb78b146f4fc7beac9674025
rienxyz <levanthere.exe@gmail.com>
2026-08-03 09:55:14 +0800

    Delete src directory

--- src/routes/directionRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const directionController = require("../controllers/directionController");
-
-router.get("/", directionController.getDirection);
-router.get("/all", directionController.getAllDirections);
-router.post("/", directionController.upsertDirection);
-
-module.exports = router;
